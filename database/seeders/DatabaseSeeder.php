6c3ae4543622b10c194aef345d25311d6747d890
Thanakorn0Khonman <thanakorn0khon@ku.th>
2023-10-10 10:14:46 +0700

    Add seeder templates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,26 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Product::factory(10)->create();
+        //Product::factory(10)->create();
 
         \App\Models\User::factory()->create([
             'username' => 'TestAccount',
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Intel Core I7 13700k',
+            'description' => '• 16 (8P+8E) Cores
+            • 24 Threads
+            • CPU Cooler NOT Included
+            • Intel UHD Graphics 770
+            • PCIe 5.0 and 4.0
+            • Compatible with B660, B760, H610, H710, H670, H770, Z690, Z790 Chipset',
+            'price' => 100.00,
+            'brand' => 'Intel',
+            'category' => 'CPU',
+            'quantity' => 100,
         ]);
     }
 }
